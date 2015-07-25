f22787c048b7d2d57df56ca6ce5e1860bdbf11e6
Ankur Huralikoppi <ankurh@ymail.com>
2015-07-25 11:40:40 -0700

    Discount Calc Initial Commit

---
+++ Discount Calculator/Discount Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Discount Calculator
+//
+//  Created by Ankur Huralikoppi on 7/25/15.
+//  Copyright (c) 2015 Perfection. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
