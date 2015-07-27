94752d5a20b44ac97b7fa6573f25a037fc65b0c8
Ankur Huralikoppi <ankurh@ymail.com>
2015-07-27 10:21:28 -0700

    Intermediate checkin: Placed the UI Components and tested the widgets

--- Discount Calculator/Discount Calculator/ViewController.swift
+++ Discount Calculator/Discount Calculator/ViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputPrice: UITextField!
+    @IBOutlet weak var discountPercent: UILabel!
+    @IBOutlet weak var discountSlider: UISlider!
+    @IBOutlet weak var discountSegments: UISegmentedControl!
+    @IBOutlet weak var amountToPayLabel: UILabel!
+    
+    
+    @IBAction func priceEntered(sender: AnyObject) {
+        
+        var enteredPrice:Int = inputPrice.text.toInt()!
+        var priceAfterDiscount = enteredPrice
+        
+    }
+    @IBAction func sliderMoved(sender: AnyObject) {
+        var currentDiscount:Float = discountSlider.value
+        inputPrice.text = NSString(format: "%.2f", discountSlider.value)
+        amountToPayLabel.text = NSString(format: "%.2f", discountSlider.value)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        discountSlider.minimumValue = 0
+        discountSlider.maximumValue = 100
     }
 
     override func didReceiveMemoryWarning() {
